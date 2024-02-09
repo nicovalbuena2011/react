879b0b72c401eb20cc0e510cfe233601d4f5acfd
nicovalbuena2011 <nicovalbuena2011@gmail.com>
2024-02-09 13:29:47 -0500

    useState complete

--- hola mundo/src/CounterApp.jsx
+++ hola mundo/src/CounterApp.jsx
@@ -8,12 +8,20 @@ export const CounterApp = ({ value }) => {
 
   const handleClick = (event) => setcount( count + 1 )
   // setcount( (c) => c + 1 )
+  const restValue = (event) => setcount( count - 1 )
+  const resetValue = (event) => setcount( value )
 
   return (
     <><h1>CounterApp</h1>
       <h2> {count} </h2>
       <button onClick={handleClick}>
         +1
+      </button>
+      <button onClick={restValue}>
+        -1
+      </button>
+      <button onClick={resetValue}>
+        reset
       </button>
     </>
   )
